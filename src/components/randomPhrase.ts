d49723525b3d4be7d437c4d81f2ec2b6d2c19072
octapf <frangipani.octavio@gmail.com>
2024-01-21 07:55:05 +0100

    fix function

--- src/components/randomPhrase.ts
+++ src/components/randomPhrase.ts
@@ -5,13 +5,5 @@ export const randomPhrase = () => {
 		'Happy, happy, happy',
 	]
 
-	console.log('====================================')
-	console.log(Math.floor(Math.random() * phrases.length + 0))
-	console.log('====================================')
-
 	return phrases[Math.floor(Math.random() * phrases.length + 0)]
 }
-
-console.log('====================================')
-console.log(randomPhrase())
-console.log('====================================')
